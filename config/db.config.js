dc3fccf34bfdbcbde0af93ba5349d55f4fea5dad
Padma K <39098374+padmakui4@users.noreply.github.com>
2018-05-09 08:41:53 -0500

    Update db.config.js

--- config/db.config.js
+++ config/db.config.js
@@ -2,4 +2,4 @@ module.exports = {
     url:"mongodb://localhost:27017/angular-app"
 }
 
-//https://www.callicoder.com/node-js-express-mongodb-restful-crud-api-tutorial/
+//https://www.callicoder.com/node-js-express-mongodb-restful-crud-api-tutorial/ 
